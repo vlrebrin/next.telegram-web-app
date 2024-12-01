9984a38366f8865decd1e1cd91b7812d6edf8d23
Dedpc <vlrebrin@yandex.ru>
2024-12-01 10:22:26 +0700

    after 0_init db

--- prisma/migrations/20240730023611_num_deleted_from/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `num` on the `Metering` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Metering" DROP COLUMN "num";
